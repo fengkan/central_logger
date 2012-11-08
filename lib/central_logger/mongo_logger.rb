ad3a5e3269f338550cd69c8a0a9d17981e442cd7
Prajna Zhang <nihilism.z@gmail.com>
2012-11-08 11:58:53 +0800

    Increase production_log capacity

--- lib/central_logger/mongo_logger.rb
+++ lib/central_logger/mongo_logger.rb
@@ -8,8 +8,8 @@ module CentralLogger
     include ReplicaSetHelper
 
     MB = 2 ** 20
-    PRODUCTION_COLLECTION_SIZE = 256 * MB
-    DEFAULT_COLLECTION_SIZE = 128 * MB
+    PRODUCTION_COLLECTION_SIZE = 2560 * MB
+    DEFAULT_COLLECTION_SIZE = 1280 * MB
     # Looks for configuration files in this order
     CONFIGURATION_FILES = ["central_logger.yml", "mongoid.yml", "database.yml"]
     LOG_LEVEL_SYM = [:debug, :info, :warn, :error, :fatal, :unknown]
